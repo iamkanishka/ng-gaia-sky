c1abfb94c4f193b5ce814a1507d9d948dadf8765
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-01-08 20:57:59 +0530

    GaiaService fns

--- src/app/data/gia.service.ts
+++ src/app/data/gia.service.ts
@@ -4,38 +4,39 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GaiaService {
-
   constructor(private http: HttpClient) {}
 
   fetchSample(limit = 500): Observable<any> {
-
     const query = `
       SELECT TOP ${limit}
-        source_id,
-        ra,
-        dec,
-        parallax,
-        pmra,
-        pmdec,
-        phot_g_mean_mag,
-        bp_rp
-      FROM gaiadr3.gaia_source
-      WHERE phot_g_mean_mag < 15
-        AND parallax > 0
+       source_id,
+       ra, dec,
+       parallax,
+       pmra, pmdec,
+       phot_g_mean_mag,
+       bp_rp,
+       ra_error,
+       dec_error,
+       pmra_error,
+       pmdec_error
+       FROM gaiadr3.gaia_source
+       WHERE phot_g_mean_mag < 15
+       AND parallax > 0
        
     `;
 
     const body =
-      "REQUEST=doQuery" +
-      "&LANG=ADQL" +
-      "&FORMAT=JSON" +
-      "&QUERY=" + encodeURIComponent(query + ";");
+      'REQUEST=doQuery' +
+      '&LANG=ADQL' +
+      '&FORMAT=JSON' +
+      '&QUERY=' +
+      encodeURIComponent(query + ';');
 
     return this.http.post('/esa-tap', body, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      responseType: 'json'
+      responseType: 'json',
     });
   }
 }
