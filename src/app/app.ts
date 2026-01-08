c985859f236ba78088c7468013ff8475dcb0e6eb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-01-08 20:51:29 +0530

    imported  inspector, time-slider, magnitude slider components and added few variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { Sky } from './sky/sky';
+import { TimeSlider } from './ui/time-slider';
+import { MagnitudeSlider } from './ui/magnitude-slider.component';
+import { InspectorComponent } from './ui/inspector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Sky],
+  imports: [Sky, TimeSlider, MagnitudeSlider, InspectorComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('gaia-sky');
+   currentYear = 2025;
+   currentMagLimit = 6.0;
+
+  onYearChange(year: any) {
+    this.currentYear = year;
+    // SkyComponent listens internally (see below)
+  }
 }
