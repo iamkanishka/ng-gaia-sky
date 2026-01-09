99c2329be21cf1aeb31cadf76cbd75c560c47428
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-01-09 23:37:10 +0530

    VRButton

---
+++ src/types/three-vrbutton.d.ts
@@ -0,0 +1,7 @@
+declare module 'three/examples/jsm/webxr/VRButton' {
+  import { WebGLRenderer } from 'three';
+
+  export class VRButton {
+    static createButton(renderer: WebGLRenderer): HTMLElement;
+  }
+}
