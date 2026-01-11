334bdbf16cb39de6c3306075b1666bb18f67cd18
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-01-11 20:28:03 +0530

    chore: Modified query

--- src/app/data/gia.service.ts
+++ src/app/data/gia.service.ts
@@ -7,23 +7,23 @@ export class GaiaService {
   constructor(private http: HttpClient) {}
 
   fetchSample(limit = 500): Observable<any> {
-    const query = `
-      SELECT TOP ${limit}
-       source_id,
-       ra, dec,
-       parallax,
-       pmra, pmdec,
-       phot_g_mean_mag,
-       bp_rp,
-       ra_error,
-       dec_error,
-       pmra_error,
-       pmdec_error
-       FROM gaiadr3.gaia_source
-       WHERE phot_g_mean_mag < 15
-       AND parallax > 0
-       
-    `;
+    const query = `SELECT TOP ${limit}
+  source_id,
+  ra, dec,
+  parallax,
+  pmra, pmdec,
+  phot_g_mean_mag,
+  bp_rp,
+  ra_error,
+  dec_error,
+  pmra_error,
+  pmdec_error
+FROM gaiadr3.gaia_source
+WHERE phot_g_mean_mag < 15
+  AND parallax > 0
+  AND ra BETWEEN 120 AND 150
+  AND dec BETWEEN -15 AND 15
+ `;
 
     const body =
       'REQUEST=doQuery' +
